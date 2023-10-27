01baa768033d98dbbe559a2869e0529d46c151dc
joseenso <joseensothethird@gmail.com>
2023-10-27 21:38:13 +0800

    benefit.jsx

--- src/components/section/benefit.jsx
+++ src/components/section/benefit.jsx
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { FaFacebook, FaTwitter, FaTelegram, FaInstagram } from "react-icons/fa"; // Import icons from react-icons
-import AOS from "aos"; // Import the react-aos library
-import "aos/dist/aos.css"; // Import the CSS for AOS
+import { FaFacebook, FaTwitter, FaTelegram, FaInstagram } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// Create a styled component for the card container
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 80vh;
   background-color: white;
 `;
 
-// Create a styled component for each card
 const Card = styled.div`
   margin: 5px;
   position: relative;
   width: 370px;
   max-width: calc(100vw - 48px);
-  min-height: 290px;
+  height: 350px;
   padding: 40px 30px 30px;
   color: #000;
   background: #fff;
-  box-shadow: 0 149px 60px rgba(0, 0, 0, 0.02), 0 84px 50px rgba(0, 0, 0, 0.08),
-    0 37px 37px rgba(0, 0, 0, 0.13), 0 9px 21px rgba(0, 0, 0, 0.15),
+  box-shadow: 0 149px 60px rgba(0, 0, 0, 0.02),
+    0 84px 50px rgba(0, 0, 0, 0.08),
+    0 37px 37px rgba(0, 0, 0, 0.13),
+    0 9px 21px rgba(0, 0, 0, 0.15),
     0 0 0 rgba(0, 0, 0, 0.16);
   border: 4px solid #fff;
   border-radius: 20px;
@@ -32,9 +32,9 @@ const Card = styled.div`
   transition: all 0.2s linear;
   border: 0.1px solid black;
   bottom: 50px;
+  display: flex;
+  flex-direction: column;
 `;
-
-// Create a styled component for the headings
 
 const Heading = styled.h2`
   font-size: 24px;
@@ -43,18 +43,20 @@ const Heading = styled.h2`
   font-family: "Gotham", sans-serif;
   font-weight: bold;
 `;
+
 const CardParagraph = styled.p`
-  margin-top: 5px;
-  font-size: 20px;
+  font-size: 15px;
+  flex-grow: 1;
 `;
+
 const SectionWithMargin = styled.section`
   margin-top: -60px;
   height: 120vh;
   width: 100vw;
-  border-top: 1px solid white; /* Add a 1px solid white border to the top */
+  border-top: 1px solid white;
   display: flex;
   flex-direction: column;
-  align-items: center; /* Center the content vertically within the container */
+  align-items: center;
 `;
 
 const Modal = styled.div`
@@ -69,7 +71,7 @@ const Modal = styled.div`
   z-index: 1000;
   border-radius: 10px;
   border: 1px solid black;
-  text-align: center; /* Center the text inside the modal */
+  text-align: center;
 `;
 
 const CloseButton = styled.button`
@@ -86,7 +88,7 @@ const CloseButton = styled.button`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 20px;
+  margin-top: auto; /* Position the buttons at the bottom */
 `;
 
 const SubscribeButton = styled.button`
@@ -98,16 +100,7 @@ const SubscribeButton = styled.button`
   padding: 10px 20px;
   cursor: pointer;
 `;
-const SocialMediaIcons = styled.div`
-  margin-top: 80px;
-`;
 
-const SocialMediaLink = styled.a`
-  text-decoration: none;
-  color: black;
-  margin: 0 10px;
-  font-size: 24px;
-`;
 const ThreeCardsComponent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -121,110 +114,52 @@ const ThreeCardsComponent = () => {
 
   return (
     <SectionWithMargin>
-      <h1
-       data-aos="fade-up"
-        style={{   
-          fontSize: "50px",
-          fontWeight: "bold",
-          fontFamily: "Gotham",
-          marginTop: " 150px",
-        }}
-      >
+      <h1 data-aos="fade-up" style={{ fontSize: "50px", fontWeight: "bold", fontFamily: "Gotham", marginTop: " 120px" }}>
         Benefits Watching on Our Website
       </h1>
       <CardContainer>
-        <Card  data-aos="fade-up">
+        <Card data-aos="fade-up">
           <Heading>BingeWatch</Heading>
-          <br />
-
-          <CardParagraph>
-            Stream movies, TV Shows, and Anime Everywhere
-          </CardParagraph>
-          <CardParagraph>From Top Rated Movies ,TV Shows</CardParagraph>
+          <CardParagraph>Stream movies, TV Shows, and Anime Everywhere</CardParagraph>
+          <CardParagraph>From Top Rated Movies, TV Shows, and Animes</CardParagraph>
           <CardParagraph>and Animes</CardParagraph>
           <ButtonContainer>
-            <a
-              href="/home"
-              style={{
-                backgroundColor: "#e50914",
-                border: "none",
-                borderRadius: "5px",
-                color: "white",
-                fontSize: "16px",
-                padding: "10px 20px",
-                textDecoration: "none", // Remove underline for the link
-                display: "inline-block", // Make the link behave like a block element
-                cursor: "pointer",
-              }}
-            >
+            <a href="/home" style={{ backgroundColor: "#e50914", border: "none", borderRadius: "5px", color: "white", fontSize: "16px", padding: "10px 20px", textDecoration: "none", display: "inline-block", cursor: "pointer" }}>
               Browse
             </a>
           </ButtonContainer>
         </Card>
-        <Card  data-aos="fade-up">
+        <Card data-aos="fade-up">
           <Heading>You Can Watch Every Stream You Like for Free</Heading>
-          <CardParagraph>
-            You can also Subscribe For $5 Subscription
-          </CardParagraph>
+          <CardParagraph>You can also Subscribe For $5 Subscription</CardParagraph>
           <CardParagraph>for Free Ads Lifetime</CardParagraph>
-          <CardParagraph>
-            You Can Download Movies, TV Shows, And More
-          </CardParagraph>
+          <CardParagraph>You Can Download Movies, TV Shows, And More</CardParagraph>
           <ButtonContainer>
-            <button
-              onClick={toggleModal}
-              style={{
-                backgroundColor: "#e50914",
-                border: "none",
-                borderRadius: "5px",
-                color: "white",
-                fontSize: "16px",
-                padding: "10px 20px",
-                cursor: "pointer",
-              }}
-            >
+            <button onClick={toggleModal} style={{ backgroundColor: "#e50914", border: "none", borderRadius: "5px", color: "white", fontSize: "16px", padding: "10px 20px", cursor: "pointer" }}>
               Subscribe
             </button>
           </ButtonContainer>
           <Modal isOpen={isModalOpen}>
             <CloseButton onClick={toggleModal}>&times;</CloseButton>
-            <CardParagraph>For 5$ </CardParagraph>
+            <CardParagraph>For $5</CardParagraph>
             <SubscribeButton>Subscribe Now</SubscribeButton>
           </Modal>
         </Card>
-        <Card  data-aos="fade-up">
+        <Card data-aos="fade-up">
           <Heading>Send Feedback/Request</Heading>
-          <br />
-
           <CardParagraph>Send Feedback to our Social Media</CardParagraph>
           <CardParagraph>Send Request to our Social Media</CardParagraph>
+          <CardParagraph>Send Request to our Social Media</CardParagraph>
           <ButtonContainer>
-            <SocialMediaIcons>
-              <SocialMediaLink
-                href="URL_TO_FEEDBACK_SOCIAL_MEDIA"
-                target="_blank"
-              >
-                <FaFacebook />
-              </SocialMediaLink>
-              <SocialMediaLink
-                href="URL_TO_REQUEST_SOCIAL_MEDIA"
-                target="_blank"
-              >
-                <FaTwitter />
-              </SocialMediaLink>
-              <SocialMediaLink
-                href="https://t.me/+LsaZCwdEMe1iNzQ1"
-                target="_blank"
-              >
-                <FaTelegram />
-              </SocialMediaLink>
-              <SocialMediaLink
-                href="URL_TO_INSTAGRAM_SOCIAL_MEDIA"
-                target="_blank"
-              >
-                <FaInstagram />
-              </SocialMediaLink>
-            </SocialMediaIcons>
+            <button onClick={toggleModal} style={{ backgroundColor: "#e50914", border: "none", borderRadius: "5px", color: "white", fontSize: "16px", padding: "10px 20px", cursor: "pointer", margin: "5px" }}>
+              <FaFacebook />
+            </button>
+            <button onClick={toggleModal} style={{ backgroundColor: "#e50914", border: "none", borderRadius: "5px", color: "white", fontSize: "16px", padding: "10px 20px", cursor: "pointer", margin: "5px" }}>
+              <FaInstagram />
+            </button>
+            <button onClick={toggleModal} style={{ backgroundColor: "#e50914", border: "none", borderRadius: "5px", color: "white", fontSize: "16px", padding: "10px 20px", cursor: "pointer", margin: "5px" }}>
+              <FaTelegram />
+            </button>
           </ButtonContainer>
         </Card>
       </CardContainer>
